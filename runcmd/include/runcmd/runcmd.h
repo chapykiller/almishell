8f090a6766243ccd081bdbb91667d5b6a73a4e56
chapykiller <chapeutyler@gmail.com>
2017-05-23 03:09:44 -0300

    Passing test cases from T1-001 to T1-008

--- runcmd/include/runcmd/runcmd.h
+++ runcmd/include/runcmd/runcmd.h
@@ -1,8 +1,53 @@
 #ifndef RUNCMD_H
 #define RUNCMD_H
 
-int runcmd(const char *, int *, const int[]);
+/* Definitions for the command line parser. */
 
-void (*runcmd_onexit)(void);
+#define RCMD_MAXARGS   1024	/* Max number of arguments. */
+#define RCMD_DELIM    " \n\t\r" /* Command line delimiters.   */
+#define RCMD_NONBLOCK '&'	/* Command line nonblock sign.*/
 
-#endif
+/* Bit mask for interpreting command execution results. */
+
+#define NORMTERM    (1 <<  8)	/* 256 */
+#define EXECOK      (1 <<  9)	/* 1024 */
+#define NONBLOCK    (1 << 10)	/* 2048 */
+#define RETSTATUS   (0xFF)	/* 255 */
+
+/*
+ *   This is the user API.
+ */
+
+/* Macros to obtain information on command execution results
+   reported by 'runcmd' call.
+
+   IS_NORMTERM(result) returns true if the subprocess has terminated
+                       normally; false otherwise.
+   IS_NONBLOCK(result) returns true if the subprocess has been executed
+                       in nonblocking mode; false otherwise.
+   IS_EXECOK(result)   returns true if 'command' has been effectively
+		       executed; false otherwise.
+                       the subprocess; false otherwise.
+   EXITSTATUS(result)  returns the exit status returned by the
+                       subproccess.
+
+*/
+
+#define IS_NORMTERM(result) ((result & NORMTERM) && 1)
+#define IS_NONBLOCK(result) ((result & NONBLOCK) && 1)
+#define EXITSTATUS(result)  ( result & RETSTATUS)
+#define IS_EXECOK(result)   ((result & EXECOK) && 1)
+
+/* Subprocess' exit status upon exec failure.*/
+
+#define EXECFAILSTATUS 127
+
+/* Run a command in a subprocess. */
+
+int runcmd (const char *command, int *result, const int *io);
+
+/* Hanlder for SIGCHLD in nonblock mode. */
+
+extern void (*runcmd_onexit)(void);
+
+#endif	/* RUNCMD_H */
--- runcmd/src/runcmd.c
+++ runcmd/src/runcmd.c
@@ -1,4 +1,4 @@
-#include <runcmd.h>
+#include <runcmd/runcmd.h>
 #include <unistd.h>
 
 #include <sys/types.h>
@@ -6,135 +6,90 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+
+/* Executes 'command' in a subprocess. Information on the subprocess execution
+   is stored in 'result' after its completion, and can be inspected with the
+   aid of macros made available for this purpose. Argument 'io' is a pointer
+   to an integer vector where the first, second and third positions store
+   file descriptors to where standard input, output and error, respective,
+   shall be redirected; if NULL, no redirection is performed. On
+   success, returns subprocess' pid; on error, returns 0. */
+
+int runcmd (const char *command, int *result, const int *io) /* ToDO: const char* */
+{
+  	int pid, status;
+  	int aux, i, tmp_result;
+  	char *args[RCMD_MAXARGS], *p, *cmd;
+
+
+  	tmp_result = 0;
+
+  	/* Parse arguments to obtain an argv vector. */
+
+  	cmd = malloc ((strlen (command)+1) * sizeof(char));
+  	/* TODO sysfail (!cmd, -1); */
+  	p = strcpy (cmd, command);
+
+  	i=0;
+  	args[i++] = strtok (cmd, RCMD_DELIM);
+  	while ((i<RCMD_MAXARGS) && (args[i++] = strtok (NULL, RCMD_DELIM)));
+  	i--;
+
+  	/* Create a subprocess. */
+
+  	pid = fork();
+/* TODO  sysfail (pid<0, -1); */
+	if(pid < 0)
+	{
+		tmp_result |= EXECFAILSTATUS;
+		pid = -1;
+	}
+
+  	else if (pid>0)			/* Caller process (parent). */
+    {
+    	aux = wait (&status);
+      	/* TODO sysfail (aux<0, -1); */
+	  	if(aux < 0)
+	  	{
+		}
+
+      	/* Collect termination mode. */
+      	if (WIFEXITED(status))
+		{
+			if(WEXITSTATUS(status) == 0)
+			{
+				tmp_result |= EXECFAILSTATUS;
+			}
+			else
+			{
+				tmp_result |= WEXITSTATUS(status);
+				tmp_result |= NORMTERM;
+				tmp_result |= EXECOK;
+			}
+		}
+  	}
+
+  	else				/* Subprocess (child) */
+    {
+      	aux = execvp (args[0], args);
+      	free (cmd);
+
+		if(aux < 0)
+			exit (0);
+
+      	exit (EXECFAILSTATUS);
+    }
+
+  	if (result)
+    	*result = tmp_result;
+
+  	free (p);
+  	return pid;			/* Only parent reaches this point. */
+}
+
+/* If function runcmd is called in non-blocking mode, then function
+   pointed by rcmd_onexit is asynchronously evoked upon the subprocess
+   termination. If this variable points to NULL, no action is performed.
+*/
 
 void (*runcmd_onexit)(void) = NULL;
-
-size_t count_non_space_sequence(const char *str) {
-	size_t count = 0, i = 0;
-
-	if(!str)
-		return 0;
-
-	/* Process command argument */
-	while(str[i] != '\0') {
-		/* Ignore whitespace */
-		if(isspace(str[i])) ++i;
-
-		/* Handle POSIX's Portable Filename Character Set */
-		else if(isalnum(str[i]) || str[i] == '.' || str[i] == '_' || str[i] == '-' || str[i] == '/') {
-			++i; ++count;
-			while(isalnum(str[i]) || str[i] == '.' || str[i] == '_' || str[i] == '-' || str[i] == '/')
-				++i;
-			break;
-		}
-	}
-
-	while(str[i] != '\0') {
-		/* Ignore whitespace sequence */
-		if(isspace(str[i])) ++i;
-
-		/* Handle string argument, can contain anything between quotes */
-		else {
-			++count;
-			if(str[i] == '\"' && (i == 0 || str[i-1] != '\\')) {
-				++i;
-				while(str[i] != '\0') {
-					if(str[i] == '\"' && str[i-1] != '\\')
-					{ ++i; break; }
-					++i;
-				}
-			}
-			else while(!isspace(str[i]) && str[i] != '\0') ++i;
-	  }
-	}
-
-	return count;
-}
-
-char **extract_program_name_and_arguments(size_t names_count, const char *str, char **name_and_args) {
-	size_t seq_begin, seq_size, i = 0, j = 0;
-
-	/* The user should allocate the buffer */
-	if(!name_and_args)
-		return NULL;
-
-	/* Process command argument */
-	while(str[i] != '\0') {
-		/* Ignore whitespace */
-		if(isspace(str[i])) ++i;
-
-		/* Handle POSIX's Portable Filename Character Set */
-		else if(isalnum(str[i]) || str[i] == '.' || str[i] == '_' || str[i] == '-' || str[i] == '/') {
-			seq_begin = i; ++i;
-			while(isalnum(str[i]) || str[i] == '.' || str[i] == '_' || str[i] == '-' || str[i] == '/')
-				++i;
-			seq_size = i - seq_begin;
-			name_and_args[j] = (char *) malloc(sizeof(char) * (seq_size + 1));
-			strncpy(name_and_args[j], &str[seq_begin], seq_size);
-			name_and_args[j][seq_size] = '\0';
-			++j;
-			break;
-		}
-	}
-
-	while(str[i] != '\0') {
-		/* Ignore whitespace sequence */
-		if(isspace(str[i])) ++i;
-
-		else if(str[i] != '\0') {
-			seq_begin = i;
-
-			/* Handle string argument, can contain anything between quotes */
-			if(str[i] == '\"' && (i == 0 || str[i-1] != '\\')) {
-				++i;
-				while(str[i] != '\0') {
-					if(str[i] == '\"' && str[i-1] != '\\')
-					{ ++i; break; }
-					++i;
-				}
-			}
-			/* Any argument */
-			else while(str[i] != '\0' && !isspace(str[i])) ++i;
-
-			seq_size = i - seq_begin;
-			name_and_args[j] = (char *) malloc(sizeof(char) * (seq_size + 1));
-			strncpy(name_and_args[j], &str[seq_begin], seq_size);
-			name_and_args[j][seq_size] = '\0';
-			++j;
-	  }
-	}
-
-	name_and_args[j] = (char *) malloc(sizeof(char));
-	name_and_args[j][0] = '\0';
-
-	return name_and_args;
-}
-
-int runcmd(const char *command, int *result, const int io[3]) {
-	/* Extract program name and arguments */
-	size_t names_count = count_non_space_sequence(command);
-	char **name_and_args = (char**) malloc(sizeof(char *) * (names_count + 1));
-	/* PID for storing fork() calls */
-	pid_t pid;
-	int exit_status;
-
-	extract_program_name_and_arguments(names_count, command, name_and_args);
-
-	pid = fork();
-	if(pid == 0) {
-		execv(name_and_args[0], &name_and_args[1]);
-		free(name_and_args);
-		exit(EXIT_FAILURE);
-	}
-	else if(pid > 0){
-      /*if(BLOCKING_MODE_FLAG)*/
-			waitpid(pid, &exit_status, 0);
-	}
-
-	if(result)
-		*result = WEXITSTATUS(exit_status);
-
-	free(name_and_args);
-	return pid;
-}
