299ce5b3160fafd34d7423e856a51960609b8cd4
oitoh <henriquecmsa@gmail.com>
2017-06-05 10:17:18 -0300

    runcmd: Setup signal handlers

--- runcmd/src/runcmd.c
+++ runcmd/src/runcmd.c
@@ -18,9 +18,26 @@ void (*runcmd_onexit)(void) = NULL;
 
 struct sigaction act, old_act;
 
-void runcmd_adapter(int dummy) {
-    runcmd_onexit();
+void runcmd_adapter(int dummy);
+
+void setup_signal_handlers()
+{
+    act.sa_handler = runcmd_adapter;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGCHLD, &act, &old_act);
+}
+
+void restore_default_handlers()
+{
     sigaction(SIGCHLD, &old_act, &act);
+}
+
+void runcmd_adapter(int dummy)
+{
+    if(runcmd_onexit)
+        runcmd_onexit();
+    restore_default_handlers();
 }
 
 /* Executes 'command' in a subprocess. Information on the subprocess execution
@@ -33,105 +50,90 @@ void runcmd_adapter(int dummy) {
 
 int runcmd (const char *command, int *result, const int *io) /* ToDO: const char* */
 {
-  	int pid, status;
-  	int aux, i, tmp_result;
-  	char *args[RCMD_MAXARGS], *p, *cmd;
+    int pid, status;
+    int aux, i, tmp_result;
+    char *args[RCMD_MAXARGS], *p, *cmd;
 
-  	tmp_result = 0;
+    tmp_result = 0;
 
-  	/* Parse arguments to obtain an argv vector. */
+    /* Parse arguments to obtain an argv vector. */
 
-  	cmd = malloc ((strlen (command)+1) * sizeof(char));
-  	/* TODO sysfail (!cmd, -1); */
-  	p = strcpy (cmd, command);
+    cmd = malloc ((strlen (command)+1) * sizeof(char));
+    /* TODO sysfail (!cmd, -1); */
+    p = strcpy (cmd, command);
 
-  	i=0;
-  	args[i++] = strtok (cmd, RCMD_DELIM);
-  	while ((i<RCMD_MAXARGS) && (args[i++] = strtok (NULL, RCMD_DELIM)));
-  	i--;
+    i=0;
+    args[i++] = strtok (cmd, RCMD_DELIM);
+    while ((i<RCMD_MAXARGS) && (args[i++] = strtok (NULL, RCMD_DELIM)));
+    i--;
 
-    if(!strcmp(args[i-1], "&"))
-    {
+    if(!strcmp(args[i-1], "&")) {
         /* TODO */
         tmp_result |= NONBLOCK;
     }
 
-  	/* Create a subprocess. */
-  	pid = fork();
+    setup_signal_handlers();
 
-	if(pid < 0)
-	{
-		tmp_result |= EXECFAILSTATUS;
-		pid = -1;
-	}
+    /* Create a subprocess. */
+    pid = fork();
 
-  	else if (pid>0)			/* Caller process (parent). */
-    {
+    if(pid < 0) {
+        tmp_result |= EXECFAILSTATUS;
+        pid = -1;
+    }
+
+    else if (pid>0) {		/* Caller process (parent). */
         if(!IS_NONBLOCK(tmp_result)) {
             aux = wait (&status);
             /* TODO sysfail (aux<0, -1); */
-            if(aux < 0)
-            {
+            if(aux < 0) {
             }
 
             /* Collect termination mode. */
-            if (WIFEXITED(status))
-            {
-                if(WEXITSTATUS(status) == 0)
-                {
+            if (WIFEXITED(status)) {
+                if(WEXITSTATUS(status) == 0) {
                     tmp_result |= EXECFAILSTATUS;
-                }
-                else
-                {
+                } else {
                     tmp_result |= WEXITSTATUS(status);
                     tmp_result |= NORMTERM;
                     tmp_result |= EXECOK;
                 }
             }
         }
-  	}
+    }
 
-  	else				/* Subprocess (child) */
-    {
+    else {			/* Subprocess (child) */
         if(IS_NONBLOCK(tmp_result)) {
             pid = fork();
             if(pid < 0) {
-                return pid;
-            }
-            else if(pid > 0) {
-                if(runcmd_onexit) {
-                    sigemptyset(&act.sa_mask);
-                    act.sa_handler = runcmd_adapter;
-                    act.sa_flags = 0;
-                    sigaction(SIGCHLD, &act, &old_act);
-                }
+                exit(EXECFAILSTATUS);
+            } else if(pid > 0) {
                 aux = waitpid(pid, &status, 0);
-                return pid;
+                exit(0);
             }
         }
 
-		if(io != NULL)
-		{
-			if(io[0] != 0)
-				dup2(io[0], 0);
-			if(io[1] != 1)
-	    		dup2(io[1], 1);
-			if(io[2] != 2)
-	    		dup2(io[2], STDERR_FILENO);
-		}
+        if(io != NULL) {
+            if(io[0] != 0)
+                dup2(io[0], 0);
+            if(io[1] != 1)
+                dup2(io[1], 1);
+            if(io[2] != 2)
+                dup2(io[2], STDERR_FILENO);
+        }
 
-      	aux = execvp (args[0], args);
-      	free (cmd);
+        aux = execvp (args[0], args);
+        free (cmd);
 
-		if(aux < 0)
-			exit (0);
+        if(aux < 0)
+            exit (0);
 
-      	exit (EXECFAILSTATUS);
+        exit (EXECFAILSTATUS);
     }
 
-  	if (result)
-    	*result = tmp_result;
+    if (result)
+        *result = tmp_result;
 
-  	free (p);
-  	return pid;			/* Only parent reaches this point. */
+    free (p);
+    return pid;			/* Only parent reaches this point. */
 }
